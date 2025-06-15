bd27124e42f9f873c0c3e723f1056254ad4b062a
diman <diman33@gmail.com>
2025-06-15 09:55:28 +0800

    Initial Commit

---
+++ hello26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello26
+//
+//  Created by dmdev on 2025/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello26/hello26App.swift
@@ -0,0 +1,17 @@
+//
+//  hello26App.swift
+//  hello26
+//
+//  Created by dmdev on 2025/6/15.
+//
+
+import SwiftUI
+
+@main
+struct hello26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
